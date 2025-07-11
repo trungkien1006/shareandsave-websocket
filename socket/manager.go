87019518eaf0089876665378f56d4fc2619afd2c
trungkien1006 <cristntk0162@gmail.com>
2025-07-11 17:25:29 +0700

    fix

--- socket/manager.go
+++ socket/manager.go
@@ -451,7 +451,8 @@ func SendNoti(ctx context.Context, notis []map[string]string) error {
 	timeLayout := "2006-01-02T15:04:05.000Z07:00"
 
 	for _, value := range notis {
-		log.Println("++++++++Time: " + value["createdAt"])
+
+		fmt.Println("++++++++Time: " + value["createdAt"])
 
 		ID, _ := strconv.Atoi(value["ID"])
 		senderID, _ := strconv.Atoi(value["senderID"])
@@ -459,7 +460,9 @@ func SendNoti(ctx context.Context, notis []map[string]string) error {
 		targetID, _ := strconv.Atoi(value["targetID"])
 		createdAt, _ := time.Parse(timeLayout, value["createdAt"])
 		isRead, _ := strconv.Atoi(value["isRead"])
-		log.Println("++++++++Time converted : " + createdAt.String())
+
+		fmt.Println("++++++++Time converted : " + createdAt.String())
+
 		domainNotis = append(domainNotis, model.NotiSend{
 			ID:           uint(ID),
 			SenderID:     uint(senderID),
